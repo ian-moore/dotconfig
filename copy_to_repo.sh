bc8675feb8269b740db36ee345b47088b5a84090
Ian Moore <1709416+ian-moore@users.noreply.github.com>
2023-08-16 21:22:52 -0500

    add Brewfile

--- copy_to_repo.sh
+++ copy_to_repo.sh
@@ -6,13 +6,21 @@ set -e
 set -o pipefail
 set -u
 
-DEBUG=${DEBUG-false}
+DEBUG=${DEBUG:-false}
 
 if [ "$DEBUG" = true ]; then
   set -x
 fi
 
 main() {
+  if [ -z "${1-}" ]; then
+    # list the available yaml files
+    find ./*.yaml | sed 's/.\/\(.*\).yaml/\1/g'
+    exit 0
+  else
+    echo "Copying ${1} files..."
+  fi
+
   local filename="$1.yaml"
   for file in $(yq '.files[]' < "${filename}")
   do
@@ -22,11 +30,10 @@ main() {
       echo "$absolute_path -> $destination_path"
       mkdir -p "$(dirname "$destination_path")"
       cp "$absolute_path" "$destination_path"
-
     fi
   done
 
-  tree -a
+  tree -a -I '.git/'
 }
 
 main "$@"
